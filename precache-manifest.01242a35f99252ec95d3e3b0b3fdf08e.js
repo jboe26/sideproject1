f1cdeafb0588920ab68ac8bd14e04fdfc9cc7341
jboe26 <joshrboepple@outlook.com>
2020-01-31 13:58:08 -0500

    Updates

---
+++ precache-manifest.01242a35f99252ec95d3e3b0b3fdf08e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161095e7e6dece9c7ba3eaf70af1c4f9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5a0e3101e86887342960",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "02443351ad28417b6084",
+    "url": "/my-app/static/css/main.97b91130.chunk.css"
+  },
+  {
+    "revision": "5a0e3101e86887342960",
+    "url": "/my-app/static/js/2.2c069c66.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/my-app/static/js/2.2c069c66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02443351ad28417b6084",
+    "url": "/my-app/static/js/main.4c14bac9.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "b258dc3f12ec4c92a990eeacc313399e",
+    "url": "/my-app/static/media/crystal.b258dc3f.gif"
+  },
+  {
+    "revision": "e26e157678bb06a07dce1e776d2f7578",
+    "url": "/my-app/static/media/demoday.e26e1576.jpg"
+  },
+  {
+    "revision": "aa8da60390eb9f62109f5e8cd70dd1b9",
+    "url": "/my-app/static/media/quiz.aa8da603.gif"
+  },
+  {
+    "revision": "aa2fc0135eaca976c23b76d8aaae154c",
+    "url": "/my-app/static/media/whatschat.aa2fc013.gif"
+  }
+]);
